7cc53d0986003ff70bc20a3bb02ddb0e238c4c6f
sinnpanther <benjcherpas@orange.fr>
2025-12-20 23:42:19 +0100

    add config

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.title = "Latente Fear"
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.vsync = 1         -- Évite les déchirements d'image
+    t.console = true           -- Très important pour voir tes erreurs et tes prints
+end
