617ddba7fc95727a138365eb3693fe2cbfb0daa6
sinnpanther <benjcherpas@orange.fr>
2025-12-21 22:43:31 +0100

    improve AI

--- src/entities/enemies/Chaser.lua
+++ src/entities/enemies/Chaser.lua
@@ -13,21 +13,58 @@ function Chaser:new(world, x, y)
 end
 
 function Chaser:update(dt, player)
-    local pPos = Vector(player.x, player.y)
-    local ePos = self.pos -- Déjà un vecteur
+    -- Position de l'ennemi
+    local ePos = Vector(self.x + self.w/2, self.y + self.h/2)
+    -- Position du joueur
+    local targetPos = Vector(player.x + player.w/2, player.y + player.h/2)
+    local dir = (targetPos - ePos):normalized()
 
-    -- 2. Calcul du vecteur de direction (Cible - Moi)
-    local diff = pPos - ePos
+    -- --- LOGIQUE D'ANTICIPATION (Le Radar) ---
+    local lookAheadDistance = 50 -- Distance de détection
+    local detectorAngle = 0.5    -- Angle des "antennes" (en radians)
 
-    -- 3. Mouvement : on normalise la direction et on applique la vitesse
-    -- .trimmed(1) gère le cas où l'ennemi est pile sur le joueur (distance 0)
-    local velocity = diff:trimmed(1) * self.speed * dt
-    local goal = ePos + velocity
+    -- On crée deux vecteurs de détection (gauche et droite)
+    local rayLeft = dir:rotated(-detectorAngle) * lookAheadDistance
+    local rayRight = dir:rotated(detectorAngle) * lookAheadDistance
 
-    -- 4. On demande à Bump de gérer les collisions
-    local ax, ay, cols, len = self.world:move(self, goal.x, goal.y, WorldUtils.enemyFilter)
+    -- On teste si un des rayons touche un obstacle via Bump
+    -- (On utilise world:querySegment pour "regarder" devant)
+    local itemsL, lenL = self.world:querySegment(ePos.x, ePos.y, ePos.x + rayLeft.x, ePos.y + rayLeft.y)
+    local itemsR, lenR = self.world:querySegment(ePos.x, ePos.y, ePos.x + rayRight.x, ePos.y + rayRight.y)
 
-    -- 5. Mise à jour des coordonnées
+    local avoidanceForce = Vector(0, 0)
+
+    -- Si le rayon gauche touche un mur, on pousse vers la droite
+    if lenL > 0 then
+        for i=1, lenL do
+            if itemsL[i].type == "wall" or itemsL[i].type == "pillar" then
+                avoidanceForce = avoidanceForce + dir:perpendicular()
+                break
+            end
+        end
+    end
+
+    -- Si le rayon droit touche un mur, on pousse vers la gauche
+    if lenR > 0 then
+        for i=1, lenR do
+            if itemsR[i].type == "wall" or itemsR[i].type == "pillar" then
+                avoidanceForce = avoidanceForce - dir:perpendicular()
+                break
+            end
+        end
+    end
+
+    -- --- CALCUL DU MOUVEMENT FINAL ---
+    -- On mélange la direction vers le joueur et la force d'évitement
+    local finalDir = (dir + avoidanceForce * 1.5):normalized()
+    local velocity = finalDir * self.speed * dt
+
+    local goalX, goalY = self.x + velocity.x, self.y + velocity.y
+
+    -- Bump sert maintenant de "filet de sécurité" final
+    local ax, ay, cols, len = self.world:move(self, goalX, goalY, WorldUtils.enemyFilter)
+
+    -- 4. Mise à jour des coordonnées
     MathUtils.updateCoordinates(self, ax, ay)
 end
 
